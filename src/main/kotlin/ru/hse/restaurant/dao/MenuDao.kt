de3a667f2805c8df81a89335e98b70ece4ab9de8
gnome-gnomych <gyrei_am@mail.ru>
2024-02-17 22:32:23 +0300

    new comments

--- src/main/kotlin/ru/hse/restaurant/dao/MenuDao.kt
+++ src/main/kotlin/ru/hse/restaurant/dao/MenuDao.kt
@@ -4,7 +4,7 @@ import ru.hse.restaurant.entity.DishEntity
 import ru.hse.restaurant.entity.MenuEntity
 
 interface MenuDao {
-    fun addDishToMenu(dish: DishEntity)
-    fun deleteDishWithMenu(dish: DishEntity)
+    fun addDishToMenu(dish: DishEntity) // add one dish to menu
+    fun deleteDishWithMenu(dish: DishEntity) // delete one dish to menu
     fun returnAllDishes() : List<DishEntity> // return all dishes
 }
